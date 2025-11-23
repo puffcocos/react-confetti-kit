17b8e5684b9b5d68751aa331411c80f68d3d53f9
owen <sohnjunior@gmail.com>
2025-11-24 00:01:10 +0900

    (config): ⚙️ GitHub Pages 배포 설정 추가
    - gh-pages 패키지 추가 및 deploy 스크립트 설정
    - basename 및 base path를 /confetti-editor/로 설정하여 GitHub Pages 배포 지원

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,9 @@
-import type { Config } from "@react-router/dev/config";
+import type { Config } from '@react-router/dev/config'
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
-} satisfies Config;
+  /**
+   * @see https://github.com/remix-run/react-router/issues/13446#issuecomment-2822107203
+   */
+  basename: '/confetti-editor/',
+  ssr: false,
+} satisfies Config
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  /**
+   * @see https://github.com/remix-run/react-router/issues/13446#issuecomment-2822107203
+   */
+  base: '/confetti-editor/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-});
+})
